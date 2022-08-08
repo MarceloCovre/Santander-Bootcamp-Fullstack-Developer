89e3d5d95aa97117e10c5c26a00f6f5b4362246f
Marcelo <marcelocovre@outlook.com>
2022-08-08 19:52:27 -0300

    banco digital java

---
+++ criando_um_banco_digital_com_java_e_orientação_a_objetos/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente marcelo = new Cliente();
+        marcelo.setNome("Marcelo");
+
+        Conta cc = new ContaCorrente(marcelo);
+        Conta poupanca = new ContaPoupanca(marcelo);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
